7a0e54cdf8aea375fb35de74a6b60cfe46f57cee
Marvin Berrio <marvinccp@gmail.com>
2024-07-19 11:16:28 +0200

    configure corse

--- src/main.ts
+++ src/main.ts
@@ -21,7 +21,7 @@ async function bootstrap() {
   app.enableCors({
     origin: 'http://localhost:5173/',
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
-    allowedHeaders:'*'
+    allowedHeaders:'Access-Control-Allow-Origin'
   })
 }
 bootstrap();
