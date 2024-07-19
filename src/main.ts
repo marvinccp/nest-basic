7afbf4aec8cc0885b2acb241257c1e8b7b74662d
Marvin Berrio <marvinccp@gmail.com>
2024-07-19 11:05:32 +0200

    configure corse

--- src/main.ts
+++ src/main.ts
@@ -19,7 +19,7 @@ async function bootstrap() {
   SwaggerModule.setup('api', app, document);
   await app.listen(process.env.PORT || 3000);
   app.enableCors({
-    origin: '*',
+    origin: 'http://localhost:5173/',
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     allowedHeaders:'*'
   })
